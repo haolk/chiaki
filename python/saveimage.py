201720f07973ea6eb830b2ce126248f37bbf7ff6
Dave <davidwu226@hotmail.com>
2022-06-28 20:45:31 -0700

    added load and save image''

---
+++ python/saveimage.py
@@ -0,0 +1,27 @@
+import zmq
+import numpy
+import cv2
+import pytesseract
+import re
+
+fcontext = zmq.Context()
+fsocket = fcontext.socket(zmq.REQ)
+fsocket.connect("tcp://localhost:5555")
+
+fsocket.send_string('0')
+msg = fsocket.recv()
+if  len(msg) != 0:
+
+    with open('img.cv', 'wb') as file:
+        file.write(msg)
+
+    height = int.from_bytes(msg[0:2], byteorder='little')
+    width = int.from_bytes(msg[2:4], byteorder='little')
+    channel = int.from_bytes(msg[4:6], byteorder='little')
+    img = numpy.frombuffer(msg, numpy.uint8, -1, 6)
+    img.shape = (height, width, channel)
+    img = img[:, :, ::-1].copy()
+
+        
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
